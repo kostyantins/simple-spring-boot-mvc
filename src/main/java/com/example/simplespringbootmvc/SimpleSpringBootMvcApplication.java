a1c18b49924b079150db2666ad9e28db1d3332f3
k.sichenko <kostyantyn@backbase.com>
2022-08-30 17:07:50 +0300

    Initial commit

---
+++ src/main/java/com/example/simplespringbootmvc/SimpleSpringBootMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplespringbootmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringBootMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringBootMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simplespringbootmvc/SimpleSpringBootMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplespringbootmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringBootMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
